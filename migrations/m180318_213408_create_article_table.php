056c8e0f2f0bfa77282b4281c6ff17cacf88599e
ERKA <batteryf@gmail.com>
2018-03-19 00:02:31 +0200

    start project, add migration

---
+++ migrations/m180318_213408_create_article_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `article`.
+ */
+class m180318_213408_create_article_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('article', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'description' => $this->text(),
+            'content' => $this->text(),
+            'date' => $this->date(),
+            'image' => $this->string(),
+            'viewed' => $this->integer(),
+            'user_id' => $this->integer(),
+            'status' => $this->integer(),
+            'category_id' => $this->integer(),
+            'keyword' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('article');
+    }
+}
---
+++ migrations/m180318_213458_create_category_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `category`.
+ */
+class m180318_213458_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('category', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'keyword' => $this->string(),
+            'description' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('category');
+    }
+}
