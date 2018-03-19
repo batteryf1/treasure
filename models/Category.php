1d7571dd3aee72ee3024bcc989633afce4a58643
ERKA <batteryf@gmail.com>
2018-03-19 02:26:13 +0200

    Final project

--- models/Category.php
+++ models/Category.php
@@ -3,14 +3,13 @@
 namespace app\models;
 
 use Yii;
+use yii\data\Pagination;
 
 /**
  * This is the model class for table "category".
  *
- * @property int $id
+ * @property integer $id
  * @property string $title
- * @property string $keyword
- * @property string $description
  */
 class Category extends \yii\db\ActiveRecord
 {
@@ -28,8 +27,7 @@ class Category extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['description'], 'string'],
-            [['title', 'keyword'], 'string', 'max' => 255],
+            [['title'], 'string', 'max' => 255],
         ];
     }
 
@@ -41,8 +39,43 @@ class Category extends \yii\db\ActiveRecord
         return [
             'id' => 'ID',
             'title' => 'Title',
-            'keyword' => 'Keyword',
-            'description' => 'Description',
         ];
+    }
+
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['category_id' => 'id']);
+    }
+
+    public function getArticlesCount()
+    {
+        return $this->getArticles()->count();
+    }
+    
+    public static function getAll()
+    {
+        return Category::find()->all();
+    }
+    
+    public static function getArticlesByCategory($id)
+    {
+        // build a DB query to get all articles
+        $query = Article::find()->where(['category_id'=>$id]);
+
+        // get the total number of articles (but do not fetch the article data yet)
+        $count = $query->count();
+
+        // create a pagination object with the total count
+        $pagination = new Pagination(['totalCount' => $count, 'pageSize'=>6]);
+
+        // limit the query using the pagination and retrieve the articles
+        $articles = $query->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        $data['articles'] = $articles;
+        $data['pagination'] = $pagination;
+        
+        return $data;
     }
 }
